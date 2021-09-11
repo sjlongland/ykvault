0925f30cf969b838f1cb48db146ec07a05bcff90
Stuart Longland <me@vk4msl.id.au>
2021-09-11 12:25:31 +1000

    otp: Decode and validate CRC16 field

--- ykvault/otp.py
+++ ykvault/otp.py
@@ -10,11 +10,12 @@ from struct import Struct
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 from .modhex import a2b_modhex, b2a_modhex
+from .crc import yubikey_crc16
 
 
 # https://developers.yubico.com/OTP/OTPs_Explained.html
 OTP_STRUCT = Struct('<6s16s')
-TOKEN_STRUCT = Struct('<6sHHBBH')
+TOKEN_STRUCT = Struct('<6sHHBBHH')
 
 
 class YubikeyToken(NamedTuple):
@@ -26,17 +27,34 @@ class YubikeyToken(NamedTuple):
     timestamp: int
     session_ctr: int
     rnd: int
+    crc: int
 
     @classmethod
     def parse(cls, token_cleartext : bytes):
+        """
+        Parse a Yubico OTP token field.
+
+        Assumes the field has been previously decrypted using a key matching
+        the AES-128 key stored in the Yubikey.  Raises ValueError if the CRC
+        is found to be incorrect, suggesting a forged OTP.
+        """
         (
                 private_uid,
                 usage_ctr,
                 timestamp_lo,
                 timestamp_hi,
                 session_ctr,
-                rnd
+                rnd,
+                crc
         ) = TOKEN_STRUCT.unpack(token_cleartext[0:TOKEN_STRUCT.size])
+
+        # Validate CRC: The checksum is verified by calculating the
+        # checksum of all bytes, including the checksum field.  This
+        # shall give a fixed residual of 0xf0b8 if the checksum is valid.
+        # If the checksum is invalid, the OTP shall be rejected.
+        computed_crc: int = yubikey_crc16(token_cleartext)
+        if computed_crc != 0xf0b8:
+            raise ValueError('Bad CRC, got 0x%04x' % computed_crc)
 
         timestamp = (timestamp_hi << 16) | timestamp_lo
 
@@ -45,7 +63,8 @@ class YubikeyToken(NamedTuple):
                 usage_ctr=usage_ctr,
                 timestamp=timestamp,
                 session_ctr=session_ctr,
-                rnd=rnd
+                rnd=rnd,
+                crc=crc
         )
 
 
@@ -55,6 +74,7 @@ class YubikeyOTP(NamedTuple):
     """
     public_uid: bytes
     token_ciphertext: bytes
+    token_cleartext: Optional[bytes]
     token: Optional[YubikeyToken]
 
     @classmethod
@@ -66,17 +86,28 @@ class YubikeyOTP(NamedTuple):
         (public_uid, token_ciphertext) = \
                 OTP_STRUCT.unpack(a2b_modhex(otp))
 
-        token: Optional[YubikeyToken] = None
-        if key is not None:
-            cipher = Cipher(algorithms.AES(key), modes.ECB()).decryptor()
-            token_cleartext: bytes = \
-                    cipher.update(token_ciphertext) \
-                    + cipher.finalize()
-
-            token = YubikeyToken.parse(token_cleartext)
-
-        return cls(
+        otp = cls(
                 public_uid=public_uid,
                 token_ciphertext=token_ciphertext,
+                token_cleartext=None,
+                token=None
+        )
+
+        if key is not None:
+            return otp.decode(key)
+        else:
+            return otp
+
+    def decode(self, key: bytes):
+        cipher = Cipher(algorithms.AES(key), modes.ECB()).decryptor()
+        token_cleartext: bytes = \
+                cipher.update(self.token_ciphertext) \
+                + cipher.finalize()
+
+        token: YubikeyToken = YubikeyToken.parse(token_cleartext)
+        return self.__class__(
+                public_uid=self.public_uid,
+                token_ciphertext=self.token_ciphertext,
+                token_cleartext=token_cleartext,
                 token=token
         )
